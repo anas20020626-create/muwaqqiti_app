241567a12ad9b62025aa37b15fde77cf5364e7ee
anas20020626-create <anas20020626@gmail.com>
2025-12-15 20:06:23 +0300

    Add files via upload

---
+++ settings_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('الإعدادات')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text('رقم التواصل: 0557779379', style: TextStyle(fontSize: 18)),
+            SizedBox(height: 10),
+            Text('المطور: أنــس مــســفر الشهراني', style: TextStyle(fontSize: 18)),
+          ],
+        ),
+      ),
+    );
+  }
+}
